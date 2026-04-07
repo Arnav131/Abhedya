250e89f044b41ed4c7b015bc68cac2e29b75a690
Bhavik Pahuja <bhavikpahuja0@gmail.com>
2026-04-07 05:30:41 +0530

    chore: switch render setup to free-tier deployment

--- api/build.sh
+++ api/build.sh
@@ -2,6 +2,13 @@
 
 set -o errexit
 
+REQUIREMENTS_FILE="${RENDER_REQUIREMENTS_FILE:-requirements.txt}"
+
+if [ ! -f "${REQUIREMENTS_FILE}" ]; then
+	echo "Requirements file not found: ${REQUIREMENTS_FILE}"
+	exit 1
+fi
+
 pip install --upgrade pip
-pip install -r requirements.txt
+pip install -r "${REQUIREMENTS_FILE}"
 python manage.py collectstatic --noinput
